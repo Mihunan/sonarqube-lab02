a8e77a9a3085bb07b1ec04c8575b40654462ced1
Mihunan <mmihunan@gmail.com>
2026-02-01 14:42:24 +0530

    fixed

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,49 +1,35 @@
-package main.java.com.example;
+package com.example;
 
 public class Calculator {
 
-    // Code Smell: Long method + high complexity
-     public int calculate(int a, int b, String op) { 
- 
-        if(op.equals("add")) { 
-            return a + b; 
-        } else if(op.equals("add-again")) { 
-            return a + b; // DUPLICATION 
-        } else if(op.equals("sub")) { 
-            return a - b; 
-        } else if(op.equals("sub-again")) { 
-            return a - b; // DUPLICATION 
-        } else if(op.equals("mul")) { 
-            return a * b; 
-        } else if(op.equals("div")) { 
-            if(b == 0) { 
-                return 0; 
-            } else { 
-                return a / b; 
-            } 
-        } else if(op.equals("mod")) { 
-            return a % b; 
-        } else if(op.equals("pow")) { 
-            int result = 1; 
-            for(int i = 0; i < b; i++) { 
-                result = result * a; 
-            } 
-            return result; 
-        } else { 
-            return 0; 
-        } 
-    
-    } 
-    public int addAgain(int a, int b) { 
-    return a + b; 
-} 
-
-    // Code Duplication (students must remove)
-    public int addNumbers(int x, int y) {
-        return x + y;
+    public int add(int a, int b) {
+        return a + b;
     }
 
-    public int sumValues(int a, int b) {
-        return a + b;
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero");
+        }
+        return a / b;
+    }
+
+    public int modulo(int a, int b) {
+        return a % b;
+    }
+
+    public int power(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
     }
 }
