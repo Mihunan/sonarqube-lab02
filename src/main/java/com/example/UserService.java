f933c9f3d35e9d9111996e3be4fb44404868976a
Mihunan <mmihunan@gmail.com>
2026-02-01 14:11:28 +0530

    Fix compilation error

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -1,90 +1,38 @@
+package com.example;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import javax.sql.DataSource;
-import java.util.Properties;
 
 public class UserService {
-    
-    // Use dependency injection for connection
-    private DataSource dataSource;
-    private static final String FIND_USER_QUERY = "SELECT * FROM users WHERE name = ?";
-    private static final String DELETE_USER_QUERY = "DELETE FROM users WHERE name = ?";
-    private static final String UPDATE_USER_QUERY = "UPDATE users SET name = ? WHERE name = ?";
-    
-    public UserService(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-    
-    // Use PreparedStatement to prevent SQL injection
+
+    private static final String DB_URL = "jdbc:mysql://localhost/db";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "password";
+
     public void findUser(String username) throws Exception {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(FIND_USER_QUERY)) {
-            
-            pstmt.setString(1, username); // Safe parameter binding
-            
-            try (ResultSet rs = pstmt.executeQuery()) {
-                while (rs.next()) {
-                    System.out.println("User found: " + rs.getString("name"));
-                }
-            }
+
+        String query = "SELECT * FROM users WHERE name = ?";
+
+        try (Connection conn =
+                     DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, username);
+            ps.executeQuery();
         }
     }
-    
-    // Fixed delete method
+
     public void deleteUser(String username) throws Exception {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(DELETE_USER_QUERY)) {
-            
-            pstmt.setString(1, username); // Safe parameter binding
-            int rowsAffected = pstmt.executeUpdate();
-            
-            System.out.println("Deleted " + rowsAffected + " user(s)");
+
+        String query = "DELETE FROM users WHERE name = ?";
+
+        try (Connection conn =
+                     DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement ps = conn.prepareStatement(query)) {
+
+            ps.setString(1, username);
+            ps.executeUpdate();
         }
-    }
-    
-    // Fixed update method
-    public void updateUser(String oldUsername, String newUsername) throws Exception {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(UPDATE_USER_QUERY)) {
-            
-            pstmt.setString(1, newUsername);
-            pstmt.setString(2, oldUsername);
-            
-            int rowsAffected = pstmt.executeUpdate();
-            System.out.println("Updated " + rowsAffected + " user(s)");
-        }
-    }
-    
-    // Factory method for creating DataSource
-    public static DataSource createDataSource() throws Exception {
-        // In real application, use connection pool like HikariCP
-        // This is simplified for the lab
-        Properties props = new Properties();
-        props.setProperty("user", System.getenv("DB_USER"));
-        props.setProperty("password", System.getenv("DB_PASSWORD"));
-        
-        // Return a simple DataSource wrapper
-        return new SimpleDataSource("jdbc:mysql://localhost/db", props);
-    }
-    
-    // Simple DataSource implementation
-    static class SimpleDataSource implements DataSource {
-        private final String url;
-        private final Properties props;
-        
-        SimpleDataSource(String url, Properties props) {
-            this.url = url;
-            this.props = props;
-        }
-        
-        @Override
-        public Connection getConnection() throws java.sql.SQLException {
-            return DriverManager.getConnection(url, props);
-        }
-        
-        // Other DataSource methods would be implemented here
-        // (simplified for example)
     }
 }
